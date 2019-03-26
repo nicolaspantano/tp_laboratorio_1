fba4d27b23b84e04b12e7e7d08c66c2bcce4fd5d
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-03-26 10:59:22 -0300

    Proyecto creado y funciones matematicas agregadas como biblioteca

---
+++ Calculadora/Funciones Matematicas.c
@@ -0,0 +1,33 @@
+int sumarNumeros(int numeroA, int numeroB)
+{
+    int resultado=numeroA+numeroB;
+    return resultado;
+}
+int restarNumeros(int numeroA, int numeroB)
+{
+    int resultado=numeroA-numeroB;
+    return resultado;
+}
+float dividirNumeros(int numeroA,int numeroB)
+{
+    float resultado=(float)numeroA/numeroB;
+    return resultado;
+}
+int multiplicarNumeros(int numeroA,int numeroB)
+{
+    int resultado=numeroA*numeroB;
+    return resultado;
+}
+int factorial(int numero)
+{ int resultado;
+    if(numero==0||numero==1)
+    {
+        resultado=1;
+    }
+    else
+    {
+        resultado=numero*factorial(numero-1);
+    }
+
+  return resultado;
+}
---
+++ Calculadora/Funciones Matematicas.h
@@ -0,0 +1,5 @@
+int sumarNumeros(int, int);
+int restarNumeros(int, int);
+float dividirNumeros(int,int);
+int multiplicarNumeros(int,int);
+int factorial(int);
