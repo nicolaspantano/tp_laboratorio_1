7d3b4ff92e02e58f83ad9e35eff8a7cd2f55dde0
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-03-27 17:14:34 -0300

    Comente las funciones

--- Calculadora/Funciones Matematicas.h
+++ Calculadora/Funciones Matematicas.h
@@ -1,6 +1,46 @@
+/** \brief Suma dos numeros
+ *
+ * \param int un numero
+ * \param int otro numero
+ * \return int el resultado de la suma
+ *
+ */
 int sumarNumeros(int, int);
+/** \brief Resta dos numeros
+ *
+ * \param int un numero
+ * \param int otro numero
+ * \return int el resultado de la resta
+ *
+ */
 int restarNumeros(int, int);
+/** \brief Divide dos numeros
+ *
+ * \param int Dividendo
+ * \param int Divisor
+ * \return float el resultado de la division
+ *
+ */
 float dividirNumeros(int,int);
+/** \brief Multiplica dos numeros
+ *
+ * \param int un numero
+ * \param int otro numero
+ * \return int El producto de la multiplicacion
+ *
+ */
 int multiplicarNumeros(int,int);
+/** \brief Saca el factorial de un numero
+ *
+ * \param int numero al que se le va a sacar el factorial
+ * \return int resultado del factorial
+ *
+ */
 int factorial(int);
+/** \brief Pide un numero entero
+ *
+ * \param texto[] char Texto que va a indicar que se pide
+ * \return int El entero que ingreso el usuario
+ *
+ */
 int pedirEntero(char texto[]);
