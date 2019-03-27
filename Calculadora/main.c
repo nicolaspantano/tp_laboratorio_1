ac205551c4305303a02fc913c623719a2ccc6024
NicolasPantano <nicolaspantano_757@hotmail.com>
2019-03-27 16:11:24 -0300

    Ya funciona el menu de opciones

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,42 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "Funciones Matematicas.h"
-int main()
-{
-    int opcion;
-    char seguir='s';
-    int numeroA;
-    int numeroB;
-    printf("1.Ingresar el operando A=X\n2.Ingresar el operando B=Y\n3.Calcular todas las operaciones\n4.Informar resultados\n5.Salir del programa\nElija una opcion: ");
-      fflush(stdin);
-      scanf("%d",&opcion);
 
-    do{
-      switch(opcion)
-      {
-        case 1:
-            numeroA=pedirEntero("Ingrese el operando A");
-            break;
-        case 2:
-            numeroB=pedirEntero("Ingrese el operando B");
-            break;
-        case 3:
-            printf("Estoy haciendo los calculos!!\n");
-            break;
-        case 4:
-            printf("Estoy informando!!\n");
-            break;
-        case 5:
-            printf("Estoy saliendo del programa!!\n");
-            break;
-        default:
-            printf("No ingreso una opcion valida\n");
-            break;
-      }
+int main(){
+
+int opcion;
+char seguir='s';
+int numeroA;
+int numeroB;
+int flag1=0;
+int flag2=0;
+
+
+    do
+    {
+        printf("Elija una opcion\n1.Ingresar 1er operando (A=X)\n2.Ingrese 2do operando (B=Y)\n3. Calcular todas las operaciones\n4.Informar resultados\n5.Salir\n");
+        opcion=pedirEntero("Ingrese una opcion: ");
+            switch(opcion)
+                {
+                case 1: numeroA=pedirEntero("Ingrese el 1er operando: ");
+                        break;
+                case 2:
+                        numeroB=pedirEntero("Ingrese el 2do operando: ");
+                        break;
+                case 3:
+                        printf("Calculando\n");
+                        break;
+                case 4:
+                        printf("Informando\n");
+                        break;
+                case 5:
+                        printf("Saliendo\n");
+                        break;
+
+                default:
+                        printf("No ingreso una opcion correcta\n");
+                        break;
+
+                }
     system("pause");
     system("cls");
 
-  }while(seguir=='s');
+
+    }while(seguir=='s');
+
+
+
 
     return 0;
 }
